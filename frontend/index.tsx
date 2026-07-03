398b7377da8cc66b94d5c03b0b51743b88d00ed8
BambooFury <demon12332100@gmail.com>
2026-07-03 23:37:45 +0300

    chore: remove music icon from plugin settings entry

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,21 +1,21 @@
-import { IconsModule, definePlugin, routerHook } from '@steambrew/client';
+import { definePlugin, routerHook } from '@steambrew/client';
 import { loadSettingsOnce, startPolling, registerLaunchStop, unregisterLaunchStop, loadIgnoredOnce } from './core/engine';
 import { SearchToast } from './core/SearchToast';
 import { SettingsContent } from './settings/SettingsContent';
 
 export default definePlugin(() => {
-  void loadSettingsOnce();
-  void loadIgnoredOnce();
-  routerHook.addGlobalComponent('GTSSearchToast', SearchToast);
-  startPolling();
-  registerLaunchStop();
-  return {
-    title: 'Game Theme Song',
-    icon: <IconsModule.Music />,
-    content: <SettingsContent />,
-    onDismount() {
-      routerHook.removeGlobalComponent('GTSSearchToast');
-      unregisterLaunchStop();
-    },
-  };
+	void loadSettingsOnce();
+	void loadIgnoredOnce();
+	routerHook.addGlobalComponent('GTSSearchToast', SearchToast);
+	startPolling();
+	registerLaunchStop();
+	return {
+		title: 'Game Theme Song',
+		icon: <></>,
+		content: <SettingsContent />,
+		onDismount() {
+			routerHook.removeGlobalComponent('GTSSearchToast');
+			unregisterLaunchStop();
+		},
+	};
 });
